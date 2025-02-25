56a7a4287e77bd555e9455ef078b4867a9ec0afd
HazelCaballero <hazelpce@gmail.com>
2025-02-24 19:13:47 -0600

    Base html index y loginPage, db.json y creacion de archivos js vacios

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-
-
 const express = require('express');
-const path = require('path');
-const cors = require('cors'); // Importar cors
-
 const app = express();
+const path = require('path');
 
-app.use(cors()); // Habilitar CORS para todas las rutas
-
+// Sirve los archivos estáticos desde el directorio 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Ruta para el archivo 'index.html' en 'public/pages'
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/pages', 'index.html'));
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
